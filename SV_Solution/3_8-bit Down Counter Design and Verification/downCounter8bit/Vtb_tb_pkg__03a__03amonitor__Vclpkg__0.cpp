1cad0b7642d0635f4f23e763369f6329ad94582a
Bhuvanesh <27bhuvanesh5@gmail.com>
2026-03-24 14:35:32 +0530

    Add files via upload

---
+++ SV_Solution/3_8-bit Down Counter Design and Verification/downCounter8bit/Vtb_tb_pkg__03a__03amonitor__Vclpkg__0.cpp
@@ -0,0 +1,46 @@
+// Verilated -*- C++ -*-
+// DESCRIPTION: Verilator output: Design implementation internals
+// See Vtb.h for the primary calling header
+
+#include "Vtb__pch.h"
+
+Vtb_tb_pkg__03a__03amonitor::Vtb_tb_pkg__03a__03amonitor(Vtb__Syms* __restrict vlSymsp, Vtb_counter_if* vif_in) {
+    VL_DEBUG_IF(VL_DBG_MSGF("+          Vtb_tb_pkg__03a__03amonitor::new\n"); );
+    // Body
+    _ctor_var_reset(vlSymsp);
+    this->__PVT__vif = vif_in;
+}
+
+void Vtb_tb_pkg__03a__03amonitor::__VnoInFunc_sample(Vtb__Syms* __restrict vlSymsp, CData/*7:0*/ &sample__Vfuncrtn) {
+    VL_DEBUG_IF(VL_DBG_MSGF("+          Vtb_tb_pkg__03a__03amonitor::__VnoInFunc_sample\n"); );
+    // Body
+    sample__Vfuncrtn = VL_NULL_CHECK(this->__PVT__vif, "tb_pkg.sv", 49)
+        ->q;
+}
+
+void Vtb_tb_pkg__03a__03amonitor::_ctor_var_reset(Vtb__Syms* __restrict vlSymsp) {
+    VL_DEBUG_IF(VL_DBG_MSGF("+          Vtb_tb_pkg__03a__03amonitor::_ctor_var_reset\n"); );
+    // Body
+    (void)vlSymsp;  // Prevent unused variable warning
+    __PVT__vif = nullptr;
+}
+
+std::string VL_TO_STRING(const VlClassRef<Vtb_tb_pkg__03a__03amonitor>& obj) {
+    VL_DEBUG_IF(VL_DBG_MSGF("+          Vtb_tb_pkg__03a__03amonitor::VL_TO_STRING\n"); );
+    // Body
+    return (obj ? obj->to_string() : "null");
+}
+
+std::string Vtb_tb_pkg__03a__03amonitor::to_string() const {
+    VL_DEBUG_IF(VL_DBG_MSGF("+          Vtb_tb_pkg__03a__03amonitor::to_string\n"); );
+    // Body
+    return ("'{"s + to_string_middle() + "}");
+}
+
+std::string Vtb_tb_pkg__03a__03amonitor::to_string_middle() const {
+    VL_DEBUG_IF(VL_DBG_MSGF("+          Vtb_tb_pkg__03a__03amonitor::to_string_middle\n"); );
+    // Body
+    std::string out;
+    out += "vif:" + VL_TO_STRING(__PVT__vif);
+    return (out);
+}
---
+++ SV_Solution/3_8-bit Down Counter Design and Verification/downCounter8bit/Vtb_tb_pkg__03a__03areference_model__Vclpkg__0.cpp
@@ -0,0 +1,48 @@
+// Verilated -*- C++ -*-
+// DESCRIPTION: Verilator output: Design implementation internals
+// See Vtb.h for the primary calling header
+
+#include "Vtb__pch.h"
+
+Vtb_tb_pkg__03a__03areference_model::Vtb_tb_pkg__03a__03areference_model(Vtb__Syms* __restrict vlSymsp) {
+    VL_DEBUG_IF(VL_DBG_MSGF("+          Vtb_tb_pkg__03a__03areference_model::new\n"); );
+    // Body
+    _ctor_var_reset(vlSymsp);
+    this->__PVT__ref_val = 0xffU;
+}
+
+void Vtb_tb_pkg__03a__03areference_model::__VnoInFunc_get_expected(Vtb__Syms* __restrict vlSymsp, CData/*0:0*/ reset, CData/*7:0*/ &get_expected__Vfuncrtn) {
+    VL_DEBUG_IF(VL_DBG_MSGF("+          Vtb_tb_pkg__03a__03areference_model::__VnoInFunc_get_expected\n"); );
+    // Body
+    this->__PVT__ref_val = ((IData)(reset) ? 0x000000ffU
+                             : (0x000000ffU & ((IData)(this->__PVT__ref_val) 
+                                               - (IData)(1U))));
+    get_expected__Vfuncrtn = this->__PVT__ref_val;
+}
+
+void Vtb_tb_pkg__03a__03areference_model::_ctor_var_reset(Vtb__Syms* __restrict vlSymsp) {
+    VL_DEBUG_IF(VL_DBG_MSGF("+          Vtb_tb_pkg__03a__03areference_model::_ctor_var_reset\n"); );
+    // Body
+    (void)vlSymsp;  // Prevent unused variable warning
+    __PVT__ref_val = 0;
+}
+
+std::string VL_TO_STRING(const VlClassRef<Vtb_tb_pkg__03a__03areference_model>& obj) {
+    VL_DEBUG_IF(VL_DBG_MSGF("+          Vtb_tb_pkg__03a__03areference_model::VL_TO_STRING\n"); );
+    // Body
+    return (obj ? obj->to_string() : "null");
+}
+
+std::string Vtb_tb_pkg__03a__03areference_model::to_string() const {
+    VL_DEBUG_IF(VL_DBG_MSGF("+          Vtb_tb_pkg__03a__03areference_model::to_string\n"); );
+    // Body
+    return ("'{"s + to_string_middle() + "}");
+}
+
+std::string Vtb_tb_pkg__03a__03areference_model::to_string_middle() const {
+    VL_DEBUG_IF(VL_DBG_MSGF("+          Vtb_tb_pkg__03a__03areference_model::to_string_middle\n"); );
+    // Body
+    std::string out;
+    out += "ref_val:" + VL_TO_STRING(__PVT__ref_val);
+    return (out);
+}
