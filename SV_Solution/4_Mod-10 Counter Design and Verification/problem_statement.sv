60a6b8deddd8dcf3a0ffd99cfe479de6f12be258
Bhuvanesh <27bhuvanesh5@gmail.com>
2026-03-25 08:42:39 +0530

    Create problem_statement.sv

---
+++ SV_Solution/4_Mod-10 Counter Design and Verification/problem_statement.sv
@@ -0,0 +1,5 @@
+Develop a structural Verilog code for a modulo-10 counter using D flip
+flops. Create an OOP-based test bench to test the counter’s function
+ality. Treat the DUT as a black box and compare the output against
+the expected values. Inputs: Clock, Reset. Expected Output: 00000000,
+00000001, 00000010, 00000011, ..., 00001000, 00001001, 00001010, 00001011.
